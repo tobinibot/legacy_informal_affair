cad54a8d3b778d574d7fa3be828875e4c80de61a
Tobin Juday <tobinibot@gmail.com>
2021-10-23 22:19:38 -0400

    added searching
    also the (disabled) bones of other filtering options

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,48 @@ import "controllers"
 
 console.log('Ran application.js')
 
+const tableRows = document.getElementById('item-table').querySelectorAll('tbody tr');
+const searchField = document.getElementById('search');
+const onlyGiftCardsCheckbox = document.getElementById('only-gc');
+const statusRadioButtons = document.querySelectorAll('input[name="status"]');
+
+function filter() {
+    const query = searchField.value.trim().toLowerCase().replaceAll(/\s+/g, ' ');
+    // const onlyGiftCards = onlyGiftCardsCheckbox.checked;
+
+    // let status = 'all';
+    // for (const radio of statusRadioButtons) {
+    //     if (radio.checked) {
+    //         status = radio.value;
+    //     }
+    // }
+
+    for (const row of tableRows) {
+        const text = row.innerText.toLowerCase();
+
+        if (text.indexOf(query) > -1) {
+            row.classList.remove('d-none');
+        } else {
+            row.classList.add('d-none');
+        }
+
+    }
+}
+
+searchField.addEventListener('keyup', function (e) {
+    filter();
+});
+
+// onlyGiftCardsCheckbox.addEventListener('change', function (e) {
+//     filter();
+// });
+
+// for (const radio of statusRadioButtons) {
+//     radio.addEventListener('change', function (e) {
+//         filter();
+//     });
+// }
+
 // document.addEventListener("turbolinks:load", function() {
 //     alert('hi');
 //     // let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
