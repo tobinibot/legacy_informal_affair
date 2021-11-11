90ef2a6c9174b217d6e5a1087733d9d3fe672f57
Tobin Juday <tobinibot@gmail.com>
2021-11-11 01:35:29 -0500

    include 2018 sales data

--- app/models/item_2018.rb
+++ app/models/item_2018.rb
@@ -3,6 +3,7 @@ class Item2018 < ApplicationRecord
 
   alias_attribute :title, :'Item Display Title'
   alias_attribute :description, :'Item Description'
+  alias_attribute :winning_bid, :'Winning Bid'
 
   has_one :donor, primary_key: 'DonorID', foreign_key: 'Donor ID'
   has_one :committee_contact, primary_key: 'CommitteeContactID', foreign_key: 'CommitteeContactID'
