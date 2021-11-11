9eda042656d00db51498443777720fe89bcd35c3
Tobin Juday <tobinibot@gmail.com>
2021-11-11 01:36:41 -0500

    label teacher donations and classroom art

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,28 @@
 module HomeHelper
+  CATEGORIES = {
+    1 => 'Classroom Art',
+    2 => 'Teacher Donation',
+    3 => 'Beauty',
+    4 => 'Birthday Party',
+    5 => 'Books',
+    6 => 'Cash Donations',
+    7 => 'Clothing/Accessories',
+    8 => 'Craft/Hobby',
+    9 => 'Electronics',
+    10 => 'Entertainment',
+    11 => 'Food and Wine',
+    12 => 'Health/Fitness',
+    13 => 'Home/Garden',
+    14 => 'Jewelry',
+    15 => 'Pets',
+    16 => 'Photography',
+    17 => 'Services',
+    18 => 'Travel/Getaway',
+    19 => 'Toys/Games',
+    20 => 'Sports',
+  }
+
+  def category(id)
+    CATEGORIES[id]
+  end
 end
